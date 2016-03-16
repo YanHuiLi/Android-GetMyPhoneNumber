95d03eca15c94a834df07ac3323a9f0b495d2540
Archer Lee <yanhui_0831@foxmail.com>
2016-03-17 07:30:37 +0800

    完成ListView简单优化工作。

--- app/src/main/java/android/archer/com/getmyphonenumber/MyAdapter.java
+++ app/src/main/java/android/archer/com/getmyphonenumber/MyAdapter.java
@@ -51,18 +51,46 @@ public class MyAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         //加载布局的权限
-        LayoutInflater inflater=LayoutInflater.from(context);
-        layout = (LinearLayout) inflater.inflate(R.layout.cell,null);
+//        LayoutInflater inflater=LayoutInflater.from(context);
+//        layout = (LinearLayout) inflater.inflate(R.layout.cell,null);
+//
+//        //初始化组件
+//        TextView nameTV= (TextView) layout.findViewById(R.id.nameId);
+//        TextView numberTv= (TextView) layout.findViewById(R.id.numberId);
+//        //填充数据进入组件
+//         nameTV.setText(lists.get(position).getName());
+//          numberTv.setText(lists.get(position).getNumber());
+//
+             ViewHolder holder;
 
-        //初始化组件
-        TextView nameTV= (TextView) layout.findViewById(R.id.nameId);
-        TextView numberTv= (TextView) layout.findViewById(R.id.numberId);
-        //填充数据进入组件
-         nameTV.setText(lists.get(position).getName());
-          numberTv.setText(lists.get(position).getNumber());
+        //做一些优化
+        if (convertView==null){
+            //如果convertview等于空值，说明需要加载convertview
+            convertView= LayoutInflater.from(context).inflate(R.layout.cell,null);
 
+             //对hold进行实例化，
+            holder=new ViewHolder();
+            holder.nameTV= (TextView) convertView.findViewById(R.id.nameId);
+            holder.numberTv= (TextView) convertView.findViewById(R.id.numberId);
 
-           return layout;
+            holder.nameTV.setText(lists.get(position).getName());
+            holder.numberTv.setText(lists.get(position).getNumber());
+             convertView.setTag(holder);
+        }else {
+
+            //如果convertview存在那么只需要引用我们储存的view
+            holder= (ViewHolder) convertView.getTag();
+        }
+           return convertView;
 
     }
+
+    private  static class ViewHolder {
+
+        TextView nameTV;
+        TextView numberTv;
+
+
+    }
+
 }
